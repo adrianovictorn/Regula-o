96f637ec75a5ca5e44cbaf1ca4d97cef5eeff0d6
Adriano Victor N. Ribeiro <115894997+adrianovictorn@users.noreply.github.com>
2025-06-13 09:26:23 -0300

    Update svelte.config.js

--- regulacao-frontend/svelte.config.js
+++ regulacao-frontend/svelte.config.js
@@ -1,14 +1,9 @@
-// front-regulacao/svelte.config.js
-import adapter from '@sveltejs/adapter-static';
+// svelte.config.js
+import adapter from '@sveltejs/adapter-node'; // Alterado
 
 const config = {
   kit: {
-    adapter: adapter({
-      // Caminho completo e absoluto para a pasta 'static' do seu projeto Spring Boot
-      out: '/home/adriano-victor/Documentos/REGULAÇÃO/regulacao-marcarcao-dev/src/main/resources/static',
-      fallback: 'index.html',
-      strict: false // Esta linha é crucial para rotas dinâmicas em SPA
-    }),
+    adapter: adapter() // Alterado para adapter-node
   }
 };
 
