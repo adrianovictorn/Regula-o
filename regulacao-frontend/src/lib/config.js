9fcded4058eb3f3e7f400885054e03b0213f3b6b
Adriano Victor N. Ribeiro <115894997+adrianovictorn@users.noreply.github.com>
2025-06-13 11:23:29 -0300

    Update config.js

--- regulacao-frontend/src/lib/config.js
+++ regulacao-frontend/src/lib/config.js
@@ -1,1 +1,8 @@
-export const API_BASE_URL = 'http://localhost:8080';
+import { browser } from '$app/environment';
+
+// Se o código estiver rodando no NAVEGADOR (browser === true), a base da URL é vazia.
+// Assim, a chamada será para "/api/...", e o Nginx cuidará do resto.
+//
+// Se o código estiver rodando no SERVIDOR (browser === false), usamos o endereço interno
+// para a comunicação entre serviços.
+export const API_BASE_URL = browser ? '' : 'http://localhost:8080';
