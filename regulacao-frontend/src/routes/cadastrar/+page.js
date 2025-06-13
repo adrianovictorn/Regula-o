7f0a90812ee7841cd9a5ff81f97328cdbc2eb989
Adriano Victor N. Ribeiro <115894997+adrianovictorn@users.noreply.github.com>
2025-06-13 09:40:53 -0300

    Update +page.js

--- regulacao-frontend/src/routes/cadastrar/+page.js
+++ regulacao-frontend/src/routes/cadastrar/+page.js
@@ -1,6 +1,10 @@
-export async function load ({ fetch }) {
-  const res = await fetch(`/api/solicitacoes`);
-  if (!res.ok) throw new Error("Não conseguiu se comunicar com o servidor !");
-  const solicitacoes = await res.json();  // já é a lista
+// Importa a URL base da sua API
+import { API_BASE_URL } from '$lib/config.js';
+
+export async function load({ fetch }) {
+  // Concatena a URL base com o endpoint da API
+  const res = await fetch(`${API_BASE_URL}/api/solicitacoes`);
+  if (!res.ok) throw new Error('Não conseguiu se comunicar com o servidor !');
+  const solicitacoes = await res.json(); // já é a lista
   return { solicitacoes };
 }
