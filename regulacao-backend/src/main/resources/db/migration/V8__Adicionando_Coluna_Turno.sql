bae3156e0ee33a011b0c094438dacb4a5762bb35
Adriano Victor N. Ribeiro <adrianovictorads@gmail.com>
2025-06-30 21:51:47 -0300

    att

--- regulacao-backend/src/main/resources/db/migration/V8__Adicionando_Coluna_Turno.sql
+++ regulacao-backend/src/main/resources/db/migration/V8__Adicionando_Coluna_Turno.sql
@@ -1,5 +1,8 @@
 -- Adiciona a coluna 'turno' na tabela de agendamentos
+ALTER TABLE agendamento_solicitacao ADD COLUMN turno VARCHAR(50);
 
+-- Define 'NAO_INFORMADO' para todos os agendamentos já existentes
+UPDATE agendamento_solicitacao SET turno = 'NAO_INFORMADO' WHERE turno IS NULL;
 
 -- Define 'NAO_INFORMADO' para todos os agendamentos já existentes
 UPDATE agendamento_solicitacao SET turno = 'NAO_INFORMADO' WHERE turno IS NULL;
