adb11848cc859e6defc65b736556fad0de4bae87
Adriano Victor N. Ribeiro <115894997+adrianovictorn@users.noreply.github.com>
2025-07-03 14:40:42 -0300

    att

--- regulacao-frontend/src/routes/exportar/+page.svelte
+++ regulacao-frontend/src/routes/exportar/+page.svelte
@@ -190,8 +190,7 @@
       hover: 'hover:bg-rose-700',
       icon: `<path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />`,
       especialidades: [
-        'CARDIOLOGISTA', 'TESTE_ERGOMETRICO', 'HOLTER', 'MAPA', 
-        'ECOCARDIOGRAMA_TRANSTORACICO_MODO_M_BIDIMENSIONAL_DOPPLER', 'CATETERISMO_CARDIACO_ESQUERDO_DIAGNOSTICO'
+        'CARDIOLOGISTA', 'TESTE_ERGOMETRICO', 'HOLTER', 'MAPA', 'CATETERISMO_CARDIACO_ESQUERDO_DIAGNOSTICO'
       ]
     },
     {
@@ -271,6 +270,16 @@
     'TOMOGRAFIA_COMPUTADORIZADA_PESCOCO',
     'TOMOGRAFIA_POR_EMISSAO_DE_POSITRONS_PET_CT',
     'TOMOMIELOGRAFIA_COMPUTADORIZADA'
+  ]
+  },
+   {
+  label: 'Ecocardiograma',
+  color: 'bg-red-700',
+  hover: 'hover:bg-red-800',
+  icon: `  <path stroke-linecap="round" stroke-linejoin="round" d="M9 8.25H7.5a2.25 2.25 0 0 0-2.25 2.25v9a2.25 2.25 0 0 0 2.25 2.25h9a2.25 2.25 0 0 0 2.25-2.25v-9a2.25 2.25 0 0 0-2.25-2.25H15M9 12l3 3m0 0 3-3m-3 3V2.25">
+`,
+  especialidades: [
+    'ECOCARDIOGRAMA_TRANSTORACICO_MODO_M_BIDIMENSIONAL_DOPPLER'
   ]
   }
   ];
